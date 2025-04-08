1647c8dc147271fa7a2855ab81f52fc152cdd874
Walter Matsuda <walter-eidi@hotmail.com>
2025-04-08 19:58:03 -0300

    iniciado componentização e estilização da biblioteca3

--- app/biblioteca/controller/mainPageController.py
+++ app/biblioteca/controller/mainPageController.py
@@ -10,7 +10,7 @@ from biblioteca.service import mainPageService as service
                                     
 def index(request):
     # View code here...
-    t = loader.get_template("biblioteca/view/mainPage.html")
+    t = loader.get_template("biblioteca/components/bookContainer.html")
     c = service.getIndex()
     return HttpResponse(t.render(c, request))
 
